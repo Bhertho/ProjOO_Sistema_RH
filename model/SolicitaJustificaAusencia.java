68336566ab14d37a3ba0fd0a68d20aa3965afb0e
Nicholas Antônio <114513802+Bergelomeu@users.noreply.github.com>
2026-03-29 18:00:33 -0300

    Add files via upload

---
+++ model/SolicitaJustificaAusencia.java
@@ -0,0 +1,55 @@
+package sistema_RH.model;
+
+import java.util.Date;
+
+public class SolicitaJustificaAusencia extends Solicitacao {
+
+	private Date dtInicio;
+	private Date dtFim;
+	private String comprovante;
+	
+	public Date getDtInicio() {
+		return dtInicio;
+	}
+	
+	public void setDtInicio(Date dtInicio) {
+		this.dtInicio = dtInicio;
+	}
+	
+	public Date getDtFim() {
+		return dtFim;
+	}
+	
+	public void setDtFim(Date dtFim) {
+		this.dtFim = dtFim;
+	}
+	
+	public String getComprovante() {
+		return comprovante;
+	}
+	
+	public void setComprovante(String comprovante) {
+		this.comprovante = comprovante;
+	}
+	
+	public void informarData(Date inicio, Date fim) {
+		this.dtInicio = inicio;
+		this.dtFim = fim;
+	}
+	
+	public void anexarComprovante(String comprovante) {
+		this.comprovante = comprovante;
+	}
+	
+	public boolean validar() {
+		if (this.dtInicio == null || this.dtFim == null) {
+			return false;
+		}
+		
+		if (this.comprovante == null || this.comprovante.isEmpty()) {
+			return false;
+		}
+		
+		return true;
+	}
+}
---
+++ model/Solicitacao.java
@@ -0,0 +1,41 @@
+package sistema_RH.model;
+
+public class Solicitacao {
+	
+	private String justificativa;
+	private String status;
+	
+	public Solicitacao() {
+}
+	public String getJustificativa() {
+		return justificativa;
+	}
+	
+	public void setJustificativa(String justificativa) {
+		this.justificativa = justificativa;	
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public void criarSolicitacao() {
+		this.status = "Pendente";
+	}
+	
+	public void alterarStatus(String novoStatus) {
+	    this.status = novoStatus;
+	}
+	
+	public void alterarJustificativa(String novaJustificativa) {	
+	    this.justificativa = novaJustificativa;
+	}
+	
+	public void cancelarSolicitacao() {
+		this.status = "Cancelada";	
+		}
+}
